effcdc9c8c9efe12b58994fc35ba65288a707dad
enrico <e.piacenti@reply.it>
2024-04-26 18:29:06 +0200

    Lite6 repo

---
+++ build/CMakeFiles/lite6_enrico_uninstall.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for lite6_enrico_uninstall.
+# This may be replaced when dependencies are built.
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/enrico/ws_moveit2/src/lite6_enrico
+# Build directory: /home/enrico/ws_moveit2/src/lite6_enrico/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
